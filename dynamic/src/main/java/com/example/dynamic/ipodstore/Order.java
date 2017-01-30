31a492de4adb85196638b330ecfd4585f742600f
Puneet Sharma <puneetsharma.1984@gmail.com>
2017-01-30 08:08:50 +0530

    Added comments

--- dynamic/src/main/java/com/example/dynamic/ipodstore/Order.java
+++ dynamic/src/main/java/com/example/dynamic/ipodstore/Order.java
@@ -17,11 +17,58 @@ public class Order {
 		this.manager = manager;
 		this.placedAt = placedAt;
 		this.quantity = quantity;
+		// calculate the lots
 		this.lots = quantity/10;
 		
 		costMatrix = new int[manager.getStoreCount()][lots];
 	}
 
+	
+	/**
+	 * 
+	 * Sub problems - Using suffix here means we take the first lot and calculate its min value
+	 * min cost of the current (nth) lot is given by, min cost of the prev suffix lot (n + 1).
+	 * n + 1 because we calculate from rigt to left
+	 * 
+	 * Guess = we guess the cost for all stores keeping track of the min cost and the index of the
+	 * store that gives the min cost for lot n
+	 * 
+	 * For all Stores = S and all order lots = N where lots = quantity /10
+	 * if n = current lot and s = current store 
+	 * 
+	 * DP[n,s] = min { DP[n+1][s] } for all S
+	 * 				+ cost(10,s)
+	 *
+	 * # number of subproblems = ns
+	 * # time/subproblem = O(1)
+	 * # Time complexity = O(ns)
+	 * 
+	 * Topological order
+	 * 	 for lot in lots[n:]
+	 * 		for store in stores[s:]
+	 * 
+	 * We keep track of the min value in the prev suffix as well as the store 
+	 * which provides this min value
+	 * 
+	 *			A     B
+	 * 		A   50    140
+	 * 		B   90    100
+	 * 
+	 * 		
+	 *     Assuming Initial min suffix cost = 0 and order is placed at B
+	 * 
+	 * 				10		20 		 30    ( <- lots remaining)
+	 * 		A	   270		180		 90
+	 * 		B      290      190      100  (At each node, we get the min cost from prev column + cost of ordering the lot from the current store.
+	 * 									   Assuming 	 So DP[B, 20] = min(DP[s][30]) for all s + cost [1, B] => 90 + 1== = 190 )
+	 * 		^
+	 * 		|
+	 * 	  stores
+	 * 
+	 *
+	 * 
+	 * @return
+	 */
 	public int bottumUpSuffix() {
 		if(!manager.canFulfillOrder(quantity)) {
 			return -1;
@@ -30,7 +77,7 @@ public class Order {
 		int prevMinSuffixCost = 0;
 		for(int rem = lots -1 ; rem >= 0; rem--) {
 			// keep track of the cur min suffix cost for the next suffix
-			int currMinSuffixCost = -1;
+			int currMinSuffixCost = -1, currMinStoreindex = -1;
 			for(int s= costMatrix.length-1; s >= 0; s--) {
 				if(manager.isStoreStockEmpty(s)) {
 					costMatrix[s][rem] = -1;
@@ -42,13 +89,14 @@ public class Order {
 				} else {
 					itemPrice = manager.getExportPrice(s);
 				}
-				
 				int suffixCost = prevMinSuffixCost + itemPrice;
 				if(currMinSuffixCost < 0 || currMinSuffixCost > suffixCost) {
+					currMinStoreindex = s;
 					currMinSuffixCost = suffixCost;
 				}
 				costMatrix[s][rem] = suffixCost;
 			}
+			manager.decrementStock(currMinStoreindex);
 			prevMinSuffixCost = currMinSuffixCost;
 		}
 		return prevMinSuffixCost;
@@ -57,7 +105,7 @@ public class Order {
 	public static void main(String[] args) {
 		StoreManager manager = new StoreManager();
 		
-		Order o1 = new Order(manager, manager.getStore(1), 110);
+		Order o1 = new Order(manager, manager.getStore(0), 110);
 		System.out.println(o1.bottumUpSuffix());
 	}
 	
